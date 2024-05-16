ad6f5133baf5ca9e2519bc650c27b0b206e83178
Zafri Wahab <133940987+zafriiii@users.noreply.github.com>
2024-05-16 06:20:45 -0700

    App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import CustomerRegister from "./pages/CustomerRegister/CustomerRegister";
 import CustomerLogin from "./pages/CustomerLogin/CustomerLogin";
+import MenuPage from "./pages/MenuPage/MenuPage";
+import HomePage from "./pages/HomePage/HomePage";
+import TableBook from "./pages/TableBook/TableBook";
 
 function App() {
 	return (
@@ -10,6 +13,9 @@ function App() {
 			<Routes>
 				<Route path="/" element={<CustomerLogin />} />
 				<Route path="/register" element={<CustomerRegister />} />
+				<Route path="/menu" element={<MenuPage />} />
+				<Route path="/" element={<HomePage />} />
+				<Route path="/book" element={<TableBook />} />
 			</Routes>
 		</Router>
 	);
