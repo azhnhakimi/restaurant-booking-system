0ad6ae0437b2dc9ecc592da84a2b262a34378b14
Zafri Wahab <133940987+zafriiii@users.noreply.github.com>
2024-05-16 21:43:01 -0700

    App.js

--- src/App.js
+++ src/App.js
@@ -6,16 +6,21 @@ import CustomerLogin from "./pages/CustomerLogin/CustomerLogin";
 import MenuPage from "./pages/MenuPage/MenuPage";
 import HomePage from "./pages/HomePage/HomePage";
 import TableBook from "./pages/TableBook/TableBook";
+import ReservationPage from "./pages/ReservationPage/ReservationPage";
+import AdminBooking from "./pages/AdminBooking";
+
 
 function App() {
 	return (
 		<Router>
 			<Routes>
-				<Route path="/" element={<CustomerLogin />} />
+				<Route path="/login" element={<CustomerLogin />} />
 				<Route path="/register" element={<CustomerRegister />} />
 				<Route path="/menu" element={<MenuPage />} />
 				<Route path="/" element={<HomePage />} />
 				<Route path="/book" element={<TableBook />} />
+				<Route path="/custreservation" element={<ReservationPage />} />
+				<Route path="/adminbooking" element={<AdminBooking />} />
 			</Routes>
 		</Router>
 	);
