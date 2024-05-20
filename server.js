0f28607153f250e61d8209627ed156c792964f02
Zafri Wahab <133940987+zafriiii@users.noreply.github.com>
2024-05-20 05:21:58 -0700

    server.js

--- server.js
+++ server.js
@@ -9,22 +9,35 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.post("/api/registerCustomer", async (req, res) => {
-	const { fullname, username, email, contactNumber, password } = req.body;
-	try {
-		await dbOperation.createCustomer({
-			fullname: fullname,
-			username: username,
-			email: email,
-			contactNumber: contactNumber,
-			password: password,
-		});
-		res.status(200).send("Customer registered successfully!");
-	} catch (error) {
-		console.log(error);
-		res.status(500).send("Error registering customer!");
-	}
+    const { fullname, username, email, contactNumber, password } = req.body;
+    try {
+        await dbOperation.createCustomer({
+            fullname: fullname,
+            username: username,
+            email: email,
+            contactNumber: contactNumber,
+            password: password,
+        });
+        res.status(200).send("Customer registered successfully!");
+    } catch (error) {
+        console.log(error);
+        res.status(500).send("Error registering customer!");
+    }
 });
 
-app.listen(API_PORT, () =>
-	console.log(`Server running on port ${API_PORT}...`)
-);
+app.post("/api/loginCustomer", async (req, res) => {
+    const { usernameOrEmail, password } = req.body;
+    try {
+        const isValidUser = await dbOperation.checkCredentials(usernameOrEmail, password);
+        if (isValidUser) {
+            res.status(200).send("Login successful!");
+        } else {
+            res.status(401).send("Invalid credentials!");
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).send("Error logging in!");
+    }
+});
+
+app.listen(API_PORT, () => console.log(`Server running on port ${API_PORT}...`));
