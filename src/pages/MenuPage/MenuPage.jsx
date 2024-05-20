bbcd9252627bd16b1f4f2bac0f0b8c94761c8fed
Zafri Wahab <133940987+zafriiii@users.noreply.github.com>
2024-05-20 05:31:11 -0700

    MenuPage.jsx

--- src/pages/MenuPage/MenuPage.jsx
+++ src/pages/MenuPage/MenuPage.jsx
@@ -1,12 +1,15 @@
 import Navbar from "../../components/Navbar/Navbar";
+import Navbar2 from '../../components/Navbar2/Navbar2';
 import styles from "./MenuPage.module.css"
 import MenuImage from '../../assets/Menu.png';
 import BackgroundOverlay from '../../assets/backgroundOverlay.png';
+import { useAuth } from '../../AuthContext';
 
 const MenuPage = () => {
+  const { isAuthenticated } = useAuth();
     return (
         <div>
-          <Navbar />
+          {isAuthenticated ? <Navbar2 /> : <Navbar />}
           <div className={styles.container}>
             <img src={MenuImage} alt="Menu" className={styles.menuImage} />
             <img src={BackgroundOverlay} alt="Background Overlay" className={styles.backgroundOverlay} />
