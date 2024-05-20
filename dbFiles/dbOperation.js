0822f589bd4b02c500f5ae7d3e0ff84647156d16
Zafri Wahab <133940987+zafriiii@users.noreply.github.com>
2024-05-20 05:24:35 -0700

    dbOperation.js

--- dbFiles/dbOperation.js
+++ dbFiles/dbOperation.js
@@ -1,22 +1,35 @@
-const config = require("./dbConfig");
 const sql = require("mssql");
+const config = require("./dbConfig");
 
 const createCustomer = async (Customer) => {
-	try {
-		let pool = await sql.connect(config);
-		let newCustomer = pool.request().query(
-			`INSERT INTO Customers VALUES
+    try {
+        let pool = await sql.connect(config);
+        let newCustomer = await pool.request().query(
+            `INSERT INTO Customers (fullname, username, email, contactNumber, password) VALUES
             (
-                '${Customer.fullname}', '${Customer.username}', '${Customer.email}', '${Customer.contactNumber}','${Customer.password}'
-            )
-            `
-		);
-		return newCustomer;
-	} catch (e) {
-		console.log(e);
-	}
+                '${Customer.fullname}', '${Customer.username}', '${Customer.email}', '${Customer.contactNumber}', '${Customer.password}'
+            )`
+        );
+        return newCustomer;
+    } catch (e) {
+        console.log(e);
+    }
+};
+
+const checkCredentials = async (usernameOrEmail, password) => {
+    try {
+        let pool = await sql.connect(config);
+        let result = await pool.request().query(
+            `SELECT * FROM Customers WHERE (username = '${usernameOrEmail}' OR email = '${usernameOrEmail}') AND password = '${password}'`
+        );
+        return result.recordset.length > 0;
+    } catch (e) {
+        console.log(e);
+        return false;
+    }
 };
 
 module.exports = {
-	createCustomer,
+    createCustomer,
+    checkCredentials,
 };
