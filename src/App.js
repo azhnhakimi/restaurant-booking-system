52435e92de11e2b950c391bee5ba06a32456c3f7
Zafri Wahab <133940987+zafriiii@users.noreply.github.com>
2024-05-20 05:25:51 -0700

    App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
+import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { AuthProvider } from './AuthContext';
 
 import "./App.css";
 import CustomerRegister from "./pages/CustomerRegister/CustomerRegister";
@@ -9,21 +13,23 @@ import TableBook from "./pages/TableBook/TableBook";
 import ReservationPage from "./pages/ReservationPage/ReservationPage";
 import AdminBooking from "./pages/AdminBooking";
 
-
 function App() {
-	return (
-		<Router>
-			<Routes>
-				<Route path="/login" element={<CustomerLogin />} />
-				<Route path="/register" element={<CustomerRegister />} />
-				<Route path="/menu" element={<MenuPage />} />
-				<Route path="/" element={<HomePage />} />
-				<Route path="/book" element={<TableBook />} />
-				<Route path="/custreservation" element={<ReservationPage />} />
-				<Route path="/adminbooking" element={<AdminBooking />} />
-			</Routes>
-		</Router>
-	);
+    return (
+        <AuthProvider>
+            <Router>
+                <ToastContainer />
+                <Routes>
+                    <Route path="/login" element={<CustomerLogin />} />
+                    <Route path="/register" element={<CustomerRegister />} />
+                    <Route path="/menu" element={<MenuPage />} />
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/book" element={<TableBook />} />
+                    <Route path="/custreservation" element={<ReservationPage />} />
+                    <Route path="/adminbooking" element={<AdminBooking />} />
+                </Routes>
+            </Router>
+        </AuthProvider>
+    );
 }
 
 export default App;
