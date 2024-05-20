4b41c57cbdac7455d0eb63338ebf39d3ab4ede50
Zafri Wahab <133940987+zafriiii@users.noreply.github.com>
2024-05-20 05:31:35 -0700

    ReservationPage.jsx

--- src/pages/ReservationPage/ReservationPage.jsx
+++ src/pages/ReservationPage/ReservationPage.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import styles from './ReservationPage.module.css';
+import Navbar from "../../components/Navbar/Navbar";
 import Navbar2 from '../../components/Navbar2/Navbar2';
-import CardComponent from '../../components/custCard/custCard'; // Adjust the import path as necessary
+import CardComponent from '../../components/custCard/custCard';
 import BackgroundOverlay from '../../assets/backgroundOverlay.png';
+import { useAuth } from '../../AuthContext';
 
 const ReservationPage = () => {
+    const { isAuthenticated } = useAuth();
     return (
         <>
-            <Navbar2 />
+            {isAuthenticated ? <Navbar2 /> : <Navbar />}
             <div className={styles.container}>
                 <h1 className={styles.header}>MY RESERVATION</h1>
                 <img src={BackgroundOverlay} alt="Background Overlay" className={styles.backgroundOverlay} />
