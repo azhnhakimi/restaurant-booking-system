114041c4ce7b50bbfe969b9f192b3fccc6a5607a
Zafri Wahab <133940987+zafriiii@users.noreply.github.com>
2024-05-20 05:27:24 -0700

    Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,39 +1,29 @@
-import styles from "./Navbar.module.css";
-import companyLogo from "../../assets/clearLogo.svg";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from './Navbar.module.css';
+import companyLogo from '../../assets/clearLogo.svg';
 
 const Navbar = () => {
-	return (
-		<>
-			<div className={styles.container}>
-				<div className={styles.leftSide}>
-					<img
-						className={styles.logo}
-						src={companyLogo}
-						alt="company logo"
-					/>
-				</div>
-				<div className={styles.rightSide}>
-					<ul className={styles.listContainer}>
-						<li className={styles.linkContainer}>
-							<a href="" className={styles.redirectLink}>
-								Home
-							</a>
-						</li>
-						<li className={styles.linkContainer}>
-							<a href="" className={styles.redirectLink}>
-								Menu
-							</a>
-						</li>
-						<li className={styles.linkContainer}>
-							<a href="" className={styles.redirectLink}>
-								Sign In
-							</a>
-						</li>
-					</ul>
-				</div>
-			</div>
-		</>
-	);
+  return (
+    <div className={styles.container}>
+      <div className={styles.leftSide}>
+        <img className={styles.logo} src={companyLogo} alt="company logo" />
+      </div>
+      <div className={styles.rightSide}>
+        <ul className={styles.listContainer}>
+          <li className={styles.linkContainer}>
+            <Link to="/" className={styles.redirectLink}>Home</Link>
+          </li>
+          <li className={styles.linkContainer}>
+            <Link to="/menu" className={styles.redirectLink}>Menu</Link>
+          </li>
+          <li className={styles.linkContainer}>
+            <Link to="/login" className={styles.redirectLink}>Sign In</Link> 
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
